b392bdd3770e3687a0adcd03b0f31c8a9b6482d6
KrzysztofSiataHAMK <amk1022390@student.hamk.fi>
2026-09-07 01:13:16 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,34 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int age = 100; 
+        
+        if (age > 0 && age < 18) 
+        { 
+            System.out.println("You are underage."); 
+        if (age >= 15) 
+            { 
+                System.out.println("You can drive a moped."); 
+            } 
+        } 
+        else if (age >= 65) 
+        { 
+            System.out.println("You are retired."); 
+        } 
+        else 
+        { 
+            System.out.println("You are an adult."); 
+        }
+        if (age == 18) {
+            System.out.println("You can drive a car.");
+        }
+        if (40 <= age && age <= 50) {
+            System.out.println("happy mid-life.");
+        }
+        if (age % 10 == 0) {
+            System.out.println("Anniversary party!!");
+        }
+        if (age == 100) {
+            System.out.println("congratulations!");
+        }
+    }
+}
